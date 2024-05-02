b91efd0955e71123687891cd80e7ece31002a684
PinkNoteX <omarfarghaly8@gmail.com>
2024-05-02 22:52:27 +0000

    Task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""return sum of all nums as float"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return sum of all nums as float"""
+    return sum(mxd_lst)
